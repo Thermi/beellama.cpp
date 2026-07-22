c702b9f88854b77f97690ed638aae99f7504f688
Andgihat <den.eremenko.1993@gmail.com>
2026-07-22 16:49:45 +0300

    model: add Nanbeige4.2 (Looped Transformer) support

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -2615,7 +2615,8 @@ uint32_t llama_context::graph_max_nodes(uint32_t n_tokens) const {
         model.arch == LLM_ARCH_KIMI_LINEAR ||
         model.arch == LLM_ARCH_QWEN35 ||
         model.arch == LLM_ARCH_QWEN35MOE ||
-        model.arch == LLM_ARCH_DEEPSEEK4) {
+        model.arch == LLM_ARCH_DEEPSEEK4 ||
+        model.arch == LLM_ARCH_NANBEIGE) {
         return std::max<uint32_t>(n_tokens * 40, 32u * model.n_tensors()) + tail_nodes;
     }
     uint32_t res = std::max<uint32_t>(1024u, 8u*model.n_tensors()) + tail_nodes;
